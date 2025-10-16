9f995f83c2494d90fedc40ad4a909088834965c0
mijan-alt <mijanigoni@gmail.com>
2025-10-16 11:23:25 +0100

    my first commit

---
+++ medusa-config.ts
@@ -0,0 +1,48 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+const paystackSecretKey = process.env.PAYSTACK_SECRET_KEY!;
+
+if (!paystackSecretKey) {
+  throw new Error('PAYSTACK_SECRET_KEY is required');
+}
+
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  },
+  modules: [
+      {
+      resolve: "./src/modules/payload",
+      options: {
+        serverUrl: process.env.PAYLOAD_SERVER_URL || "http://localhost:8000",
+        apiKey: process.env.PAYLOAD_API_KEY,
+        userCollection: process.env.PAYLOAD_USER_COLLECTION || "users",
+      },
+    },
+
+   {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          // other payment providers like stripe, paypal etc
+          {
+            resolve: "medusa-payment-paystack",
+            options: {
+              secret_key:paystackSecretKey,
+            } satisfies import("medusa-payment-paystack").PluginOptions,
+          },
+        ],
+      },
+    },
+  ],
+})
